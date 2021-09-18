f219d1ccc0a18f0f0b2330356fc43d3d01b62afc
name <email>
2021-09-18 21:16:17 +0800

    clean

--- src/api/login.js
+++ src/api/login.js
@@ -5,7 +5,6 @@ const jwt = require('jsonwebtoken');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const router = express.Router();
-const randtoken = require('rand-token')
 
 router.post('/login', async (req, res) => {
   const { email, password } = req.body;
