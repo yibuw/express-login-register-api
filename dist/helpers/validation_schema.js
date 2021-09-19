7e460c8085ea6f50ef28acbf27eee2f1b62c1a77
name <email>
2021-09-19 12:58:28 +0800

    update code

--- dist/helpers/validation_schema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authSchema = void 0;
-const Joi = require('joi');
-exports.authSchema = Joi.object({
-    email: Joi.string().email().lowercase().required(),
-    password: Joi.string().min(6).required(),
-    name: Joi.string().trim().lowercase(),
-});
