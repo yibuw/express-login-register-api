7562fb1dd34adbe99c4baa05dacbd825e3cc1bd8
name <email>
2021-09-19 16:41:22 +0800

    add docker file

---
+++ prisma/migrations/20210919083318_new/migration.sql
@@ -0,0 +1,52 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(200) NOT NULL,
+    "name_norm" VARCHAR(200) NOT NULL,
+    "email" VARCHAR(200) NOT NULL,
+    "email_norm" VARCHAR(200) NOT NULL,
+    "password" VARCHAR(200) NOT NULL,
+    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "product" (
+    "id" TEXT NOT NULL,
+    "SKU" TEXT NOT NULL,
+    "quantity" INTEGER NOT NULL DEFAULT 0,
+    "name" VARCHAR(200) NOT NULL,
+    "price" INTEGER NOT NULL DEFAULT 999,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "categoryId" INTEGER NOT NULL,
+
+    CONSTRAINT "product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_norm_key" ON "user"("email_norm");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "product_SKU_key" ON "product"("SKU");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "product_name_key" ON "product"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "category_name_key" ON "category"("name");
+
+-- AddForeignKey
+ALTER TABLE "product" ADD CONSTRAINT "product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
